cf2fb75d336962bd9b0effe187db2756bfe2f625
darkAlert <darkangel-nwo@yandex.ru>
2018-11-24 13:15:04 +0300

    Update prepare_csv.py

--- data_provider/prepare_csv.py
+++ data_provider/prepare_csv.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 
 
 class CsvObject:
@@ -80,27 +81,45 @@ def write_csv(samples, csv_path, data_path = '', delimiter = ',', header = None)
 	return count
 
 
+def parse_pickle(pickle_path):
+	"""Extract paths and labels from pickle-file"""
+	pickle_data = pickle.load(open(pickle_path, 'rb'))
+	paths = []
+	labels = []
+
+	for pic_path, pick_label in pickle_data.items():
+		paths.append(pic_path)
+		pick_label = pick_label.flatten()
+		label = []
+		for l in pick_label:
+			label.append(l)
+		labels.append(label)
+
+	return paths, labels
+
+
 
 def main():
 	csv_objs = []
 	csv_objs.append(CsvObject('/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/result-from-toloka/fixing.csv',
-		                      name = 'fixing', label_dict = {0:'no', 1:'yes'}))
+							  name = 'fixing', label_dict = {0:'no', 1:'yes'}))
 	csv_objs.append(CsvObject('/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/result-from-toloka/fringe.csv',
-		                      name = 'fringe', label_dict = {0:'no', 1:'yes'}))
+							  name = 'fringe', label_dict = {0:'no', 1:'yes'}))
 	csv_objs.append(CsvObject('/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/result-from-toloka/length.csv',
-		                      name = 'length', label_dict = {0:'bald', 1:'one_cm', 2:'short', 3:'long'}))
+							  name = 'length', label_dict = {0:'bald', 1:'one_cm', 2:'short', 3:'long'}))
 	csv_objs.append(CsvObject('/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/result-from-toloka/parting.csv',
-		                      name = 'parting', label_dict = {0:'none', 1:'side', 2:'centre'}))
+							  name = 'parting', label_dict = {0:'none', 1:'side', 2:'centre'}))
 	csv_objs.append(CsvObject('/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/result-from-toloka/srtucture.csv',
-		                      name = 'srtucture', label_dict = {0:'curls', 1:'dreadlocks', 2:'other'}))
-      
+							  name = 'srtucture', label_dict = {0:'curls', 1:'dreadlocks', 2:'other'}))
+	  
 	samples, rejected = merge_csv(csv_objs)
 	header = generate_header(csv_objs)
-	count = write_csv(samples, '/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/hair-attr5-full.csv',
-		              '/home/darkalert/Desktop/face_prod/normalized/2018-09-23/', header = header)
+	count = write_csv(samples, 
+		              csv_path = '/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/hair-attr5-full.csv',
+					  data_path = '/home/darkalert/MirrorJob/Datasets/Processed/hairs-v2/hair-2018-09-23/data/',
+					  header = header)
 	print('accepted samples:', len(samples), ', rejected:', len(rejected))
 	print('recorded samples:', count, ', skipped:', len(samples) - count)
-
 
 if __name__ == '__main__':
 	main()
